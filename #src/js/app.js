a2eaf424068061edede9e6ec4c2bdce515770918
Boris <boris021@mail.ru>
2021-10-21 16:13:21 +0500

    fix whatsapp

--- #src/js/app.js
+++ #src/js/app.js
@@ -1,5 +1,4 @@
 @@include('files/regular.js', {})
+@@include('files/dynamic_adapt.js', {})
 @@include('files/functions.js', {})
-@@include('files/script.js', {})
 @@include('files/forms.js', {})
-@@include('files/scroll.js', {})
